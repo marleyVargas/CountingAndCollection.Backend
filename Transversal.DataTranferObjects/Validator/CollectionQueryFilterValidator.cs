04ef218ee5e51f1c4806588be748f80cadea424e
Marley Vargas <mvargas@gmstek.com>
2023-10-26 13:38:50 -0500

    add validator

--- Transversal.DataTranferObjects/Validator/CollectionQueryFilterValidator.cs
+++ Transversal.DataTranferObjects/Validator/CollectionQueryFilterValidator.cs
@@ -6,6 +6,9 @@ namespace Transversal.Validator
 {
     public class CollectionQueryFilterValidator : AbstractValidator<CollectionQueryFilter>
     {
-       
+        public CollectionQueryFilterValidator()
+        { 
+            RuleFor(c => c.Station).MaximumLength(20).WithMessage("Station must not exceed 20 characters.");
+        }
     }
 }
